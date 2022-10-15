360347805970597b5d40f4cba4df8b06d7b1cae5
JingHong Huang <jinghong@bu.edu>
2022-10-15 15:22:55 -0400

    fixing the icons and the search bar size

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,20 +6,35 @@ import {makeStyles} from '@material-ui/core/styles'
         display: "flex",
         flexDirection: "column", 
         alignItems: "center", 
-        padding: "0 5rem 0 5rem",
-        fullWidth: true 
+        padding: "0 5rem 0 5rem"
      }
 }) 
 
-// creating an interactive search bar component in the center of the page
-function SearchBar() {
-    const classes = styles()
-    const [search, setSearch] = useState("")
+// creating a search bar component in the center of the page
+
+function SearchBar(props) {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.search(searchTerm)
+    }
+    const classes = styles() ;
     return (
-        <div className={classes.wrapper}>
-            <input value={search} onChange={e => setSearch(e.target.value)}/>
+        <div className= {classes.wrapper}>
+            <h1> Product Search </h1>
+            {/*creating a form and make the search bar customizable  */}
+            <form onSubmit={handleSubmit}>
+                {/* Make the search bar bigger  */}
+                <input type="text" value={searchTerm} onChange={handleChange} placeholder= "Anything comes to your mind, we got it (maybe)" fullWidth />
+                <input type="submit" value="Search" fullWidth/>
+            </form>
         </div>
     )
 }
-
+// exporting the search bar component
 export default SearchBar
