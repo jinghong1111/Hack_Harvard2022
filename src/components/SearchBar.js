85c3723f0cf71828bf77fefb5ed17df1589078b1
JingHong Huang <jinghong@bu.edu>
2022-10-15 15:37:53 -0400

    minor adjustment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ import {makeStyles} from '@material-ui/core/styles'
         alignItems: "center", 
      }, 
      bigSpace: {
-        marginTop: "2rem"
+        marginTop: "5rem"
         
       },
 }) 
