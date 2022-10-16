22d1535c8934307626c3b2b3de04faac61fc1599
Andre <andrecl@bu.edu>
2022-10-15 23:23:25 -0400

    Implemented Search Product form on both React and Flask

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,22 @@
-from flask import Flask
-import scraper
+from flask import Flask, request
+import scraper, json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def hello_world():
    return "<p>Hello, World!</p>"
 
-@app.route('/search')
+# takes form input and returns price results
+@app.route('/search', methods=['GET', 'POST'])
+@cross_origin()
 def getProduct():
-    product_name = request.json['product_name']
-    return scraper.getData(product_name)
-
-    
+   data = json.loads(request.data)
+   print("request received json: "+str(data))
+   product = data['product']
+   user = data['user']
+   print("product name="+product+"|user="+user)
+   return scraper.getData(product)
