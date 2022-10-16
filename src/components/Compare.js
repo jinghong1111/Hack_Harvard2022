e6ea939dacaa6ea715c965efeded1924ceb1e26b
JingHong Huang <jinghong@bu.edu>
2022-10-16 04:02:59 -0400

    undoing

---
+++ src/components/Compare.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductComparison from '/components/ProductComparison';
+
+class Compare extends React.Component {
+  render() {
+    return (
+      <ProductComparison />
+    );
+  }
+}
+
+export default (Compare);
