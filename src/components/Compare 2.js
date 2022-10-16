a8adb78736c0ca94d596bec9b19337b909f37f80
JingHong Huang <jinghong@bu.edu>
2022-10-16 06:38:12 -0400

    deleting uncessarily things

---
+++ src/components/Compare 2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductComparison from './ProductComparison';
+
+class Compare extends React.Component {
+  render() {
+    return (
+      <ProductComparison />
+    );
+  }
+}
+
+export default (Compare);
